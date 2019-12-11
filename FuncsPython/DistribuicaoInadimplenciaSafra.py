36fc4e4dca8c2386dd2ce2f0d983abeeb2a17969
Victor Almeida <vala@cesar.school>
2019-12-11 17:56:36 -0300

    Adding collors to Functions

--- FuncsPython/DistribuicaoInadimplenciaSafra.py
+++ FuncsPython/DistribuicaoInadimplenciaSafra.py
@@ -8,7 +8,7 @@ def distInadimplenciaScore(data, flag):
     colors=["rgba(0, 33, 64,1)", "rgba(206, 21, 67,1)"]
 
      # Texto da Tabela descrevendo gráfico
-    desc = "No gráfico de distribuição e taxa de inadimplência por safra observamos nas barras em azul a representatividade de cada mês utilizado na análise. A taxa em vermelho significa a inadimplência referente a cada mês."
+    static_desc = "No gráfico de distribuição e taxa de inadimplência por safra observamos nas barras em azul a representatividade de cada mês utilizado na análise. A taxa em vermelho significa a inadimplência referente a cada mês."
 
     flag = flag.strip().lower()
 
@@ -28,7 +28,7 @@ def distInadimplenciaScore(data, flag):
                 align="left"
             ),
             cells=dict(
-                values=[desc],
+                values=[static_desc],
                 align = "left")
         ),
         row=1, col=1
@@ -112,69 +112,254 @@ def distInadimplenciaScore(data, flag):
 
         pre_fig.show()
 
-
-        Titulo = str(input("Tipo da Descrição: "))
+        static_desc = static_desc
+        Titulo = str(input("Tipo da Descrição [1 - Oportunidade], [2 - Alerta], [3 - Risco]: "))
         analise = str(input("digite a descrição desejada: "))
 
-        # Fazendo os subplots para colocar a descrição e o gráfico na mesma imagem
-        fig = make_subplots(
-        rows=1, cols=3,
-        specs= [[{"type": "table"},{"secondary_y": True,"colspan": 2},None]]
-        )
-
-        # Add Table
-        fig.add_trace(
-        go.Table(
-            header=dict(
-                values=[Titulo],
-                font=dict(size=10),
-                align="left"
+        if Titulo == "1":
+            Titulo = "Oportunidade"
+            
+            fig = make_subplots(
+            rows=1, cols=3,
+            specs= [[{"type": "table"},{"secondary_y": True,"colspan": 2},None]]
+            )
+            
+            # Add Table
+            fig.add_trace(                   
+            go.Table(
+                header=dict(
+                    values=["Descrição do Gráfico"],
+                    font=dict(size=12),
+                    align="left"
+                ),
+                cells=dict(
+                    values=[static_desc],
+                    line_color='darkslategray',
+                    align = "left")
             ),
-            cells=dict(
-                values=[[desc,analise]],
-                align = "left")
-        ),
-        row=1, col=1
-        )
+            row=1, col=1
+            )
 
-        #Add bar chart
-        fig.add_trace(
-            go.Bar(x=data.MES_ANO, y=data.SAFRA,
-            customdata= pd.Series(["{0:.2f}%".format(val * 100) for val in data['SAFRA']], index = data.SAFRA),
-            marker_color=colors[0], name="Safra"), secondary_y = False, row=1, col=2
-        )
+            fig.add_trace(                   
+            go.Table(
+                  columnorder = [1,2],
+                  columnwidth = [80,400],
+                header=dict(
+                    values=[Titulo],
+                    font=dict(size=12),
+                    line_color='darkslategray',
+                    fill=dict(color=['lightgreen']),
+                    align="left"
+                ),
+                cells=dict(
+                    values=[analise],
+                    line_color='darkslategray',
+                    fill=dict(color=['lightgreen']),
+                    align ="left"
+                    )
+            ),
+            row=1, col=1
+            )
 
-        #Add line chart
-        fig.add_trace(
-            go.Scatter(x=data.MES_ANO, y=data.TAXA_INADIMPLENCIA,
-            customdata= pd.Series(["{0:.2f}%".format(val * 100) for val in data['TAXA_INADIMPLENCIA']], index = data.TAXA_INADIMPLENCIA),
-            hovertemplate = "Taxa de Inadimplência no período: %{customdata}",
-            marker_color=colors[1], name="Taxa de Inadimplência"), secondary_y = True, row=1, col=2
-        )
+            #Add bar chart
+            fig.add_trace(
+                go.Bar(x=data.MES_ANO, y=data.SAFRA,
+                customdata= pd.Series(["{0:.2f}%".format(val * 100) for val in data['SAFRA']], index = data.SAFRA),
+                marker_color=colors[0], name="Safra"), secondary_y = False, row=1, col=2
+            )
 
-        fig.update_xaxes(title_text="Período", row=1, col=2)
+            #Add line chart
+            fig.add_trace(
+                go.Scatter(x=data.MES_ANO, y=data.TAXA_INADIMPLENCIA,
+                customdata= pd.Series(["{0:.2f}%".format(val * 100) for val in data['TAXA_INADIMPLENCIA']], index = data.TAXA_INADIMPLENCIA),
+                hovertemplate = "Taxa de Inadimplência no período: %{customdata}",
+                marker_color=colors[1], name="Taxa de Inadimplência"), secondary_y = True, row=1, col=2
+            )
 
-        # Double Y Axis
-        fig.update_yaxes(tickformat=".2%",title_text="Safra", secondary_y = False, row=1, col=2)
-        fig.update_yaxes(tickformat=".2%",title_text="Taxa de Inadimplência", secondary_y = True, row=1, col=2)
+            fig.update_xaxes(title_text="Período", row=1, col=2)
 
-        #Add títulos
-        fig.update_layout(
-            title= {
-                'text': "Distribuição e Taxa de Inadimplência por Safra",
-                'y':0.9,
-                'x':0.5,
-                'xanchor': 'center',
-                'yanchor': 'top'
-            }
-        )
+            # Double Y Axis
+            fig.update_yaxes(tickformat=".2%",title_text="Safra", secondary_y = False, row=1, col=2)
+            fig.update_yaxes(tickformat=".2%",title_text="Taxa de Inadimplência", secondary_y = True, row=1, col=2)
 
-        fig.show()
+            #Add títulos
+            fig.update_layout(
+                title= {
+                    'text': "Distribuição e Taxa de Inadimplência por Safra",
+                    'y':0.9,
+                    'x':0.5,
+                    'xanchor': 'center',
+                    'yanchor': 'top'
+                }
+            )
 
-        return fig
+            fig.show()
+
+            return fig
+        elif Titulo == "2":
+            Titulo = "Alerta"
+            
+            fig = make_subplots(
+            rows=1, cols=3,
+            specs= [[{"type": "table"},{"secondary_y": True,"colspan": 2},None]]
+            )
+            
+            # Add Table
+            fig.add_trace(                   
+            go.Table(
+                header=dict(
+                    values=["Descrição do Gráfico"],
+                    font=dict(size=12),
+                    align="left"
+                ),
+                cells=dict(
+                    values=[static_desc],
+                    line_color='darkslategray',
+                    align = "left")
+            ),
+            row=1, col=1
+            )
+
+            fig.add_trace(                   
+            go.Table(
+                  columnorder = [1,2],
+                  columnwidth = [80,400],
+                header=dict(
+                    values=[Titulo],
+                    font=dict(size=12),
+                    line_color='darkslategray',
+                    fill=dict(color=['yellow']),
+                    align="left"
+                ),
+                cells=dict(
+                    values=[analise],
+                    line_color='darkslategray',
+                    fill=dict(color=['yellow']),
+                    align ="left"
+                    )
+            ),
+            row=1, col=1
+            )
+
+            #Add bar chart
+            fig.add_trace(
+                go.Bar(x=data.MES_ANO, y=data.SAFRA,
+                customdata= pd.Series(["{0:.2f}%".format(val * 100) for val in data['SAFRA']], index = data.SAFRA),
+                marker_color=colors[0], name="Safra"), secondary_y = False, row=1, col=2
+            )
+
+            #Add line chart
+            fig.add_trace(
+                go.Scatter(x=data.MES_ANO, y=data.TAXA_INADIMPLENCIA,
+                customdata= pd.Series(["{0:.2f}%".format(val * 100) for val in data['TAXA_INADIMPLENCIA']], index = data.TAXA_INADIMPLENCIA),
+                hovertemplate = "Taxa de Inadimplência no período: %{customdata}",
+                marker_color=colors[1], name="Taxa de Inadimplência"), secondary_y = True, row=1, col=2
+            )
+
+            fig.update_xaxes(title_text="Período", row=1, col=2)
+
+            # Double Y Axis
+            fig.update_yaxes(tickformat=".2%",title_text="Safra", secondary_y = False, row=1, col=2)
+            fig.update_yaxes(tickformat=".2%",title_text="Taxa de Inadimplência", secondary_y = True, row=1, col=2)
+
+            #Add títulos
+            fig.update_layout(
+                title= {
+                    'text': "Distribuição e Taxa de Inadimplência por Safra",
+                    'y':0.9,
+                    'x':0.5,
+                    'xanchor': 'center',
+                    'yanchor': 'top'
+                }
+            )
+
+            fig.show()
+
+            return fig
+
+        if Titulo == "3":
+            Titulo = "Alerta"
+            
+            fig = make_subplots(
+            rows=1, cols=3,
+            specs= [[{"type": "table"},{"secondary_y": True,"colspan": 2},None]]
+            )
+            
+            # Add Table
+            fig.add_trace(                   
+            go.Table(
+                header=dict(
+                    values=["Descrição do Gráfico"],
+                    font=dict(size=12),
+                    align="left"
+                ),
+                cells=dict(
+                    values=[static_desc],
+                    line_color='darkslategray',
+                    align = "left")
+            ),
+            row=1, col=1
+            )
+
+            fig.add_trace(                   
+            go.Table(
+                  columnorder = [1,2],
+                  columnwidth = [80,400],
+                header=dict(
+                    values=[Titulo],
+                    font=dict(size=12),
+                    line_color='darkslategray',
+                    fill=dict(color=['red']),
+                    align="left"
+                ),
+                cells=dict(
+                    values=[analise],
+                    line_color='darkslategray',
+                    fill=dict(color=['red']),
+                    align ="left"
+                    )
+            ),
+            row=1, col=1
+            )
+
+            #Add bar chart
+            fig.add_trace(
+                go.Bar(x=data.MES_ANO, y=data.SAFRA,
+                customdata= pd.Series(["{0:.2f}%".format(val * 100) for val in data['SAFRA']], index = data.SAFRA),
+                marker_color=colors[0], name="Safra"), secondary_y = False, row=1, col=2
+            )
+
+            #Add line chart
+            fig.add_trace(
+                go.Scatter(x=data.MES_ANO, y=data.TAXA_INADIMPLENCIA,
+                customdata= pd.Series(["{0:.2f}%".format(val * 100) for val in data['TAXA_INADIMPLENCIA']], index = data.TAXA_INADIMPLENCIA),
+                hovertemplate = "Taxa de Inadimplência no período: %{customdata}",
+                marker_color=colors[1], name="Taxa de Inadimplência"), secondary_y = True, row=1, col=2
+            )
+
+            fig.update_xaxes(title_text="Período", row=1, col=2)
+
+            # Double Y Axis
+            fig.update_yaxes(tickformat=".2%",title_text="Safra", secondary_y = False, row=1, col=2)
+            fig.update_yaxes(tickformat=".2%",title_text="Taxa de Inadimplência", secondary_y = True, row=1, col=2)
+
+            #Add títulos
+            fig.update_layout(
+                title= {
+                    'text': "Distribuição e Taxa de Inadimplência por Safra",
+                    'y':0.9,
+                    'x':0.5,
+                    'xanchor': 'center',
+                    'yanchor': 'top'
+                }
+            )
+
+            fig.show()
+
+            return fig
 
     else:
-        raise "Flag invalida, por favor digite yes ou no"
+        raise Exception("Flag invalida, por favor digite yes ou no")
 
 def main():
     lst = ["jan/18","fev/18","mar/18","abr/18","mai/18","jun/18","jul/18","ago/18","set/18","out/18","nov/18","dez/18","jan/19","fev/19","mar/19"]
